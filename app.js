4ec9789c7342954812c407b5452d25f16b25db88
alex chong <jihoonb7@gmail.com>
2018-10-30 16:28:23 -0700

    completed adding individual item

--- app.js
+++ app.js
@@ -7,17 +7,47 @@ update display with new text value
  */
 
 $(document).ready(function(){
+
   console.log("before\n", window.localStorage);
+  var makeIDGenerator = function(lastID) {
+    let currentID = lastID || 0;
+    return function() {
+      return ++currentID
+    }
+  }
+  //write a utility function
+  var iterator = function(array) {
+    //clear the .show-text
+    $(".show-text").empty();
+    //iterate through storage
+    for (var i = 0; i < array.length; i++) {
+      //append lis as itereate.
+      $('.show-text').append('<li>' + array.getItem(array.key(i)) + '</li>')
+    }
+      //NOTE: run this on page loads and when i add a new item.
+  }
+   
 
   // add event listener
   $(".add-text-btn").on("click", function(){
-    $(".show-text").empty();
+    //$(".show-text").empty();
+    //console.log("newID = ", getID());
     var curTextValue = $('#theKey').val(); // reading from <input>
-    var curKeyValue = "theKey"; // change to dynamic key?
+    if (localStorage.length > 0){
+      var getID = makeIDGenerator(localStorage.length-1)
+      var curKeyValue = "theKey" + getID(); // change to dynamic key?
+    } else {
+      var getID = makeIDGenerator(localStorage.length-1)
+      var curKeyValue = "theKey" + getID()
+    }
     localStorage.setItem(curKeyValue, curTextValue);
-    $(".show-text").append(curTextValue);
-  });
 
+    //$('.show-text').append('<li>' + curTextValue + '</li>');
+    //console.log(localStorage[0])
+    iterator(localStorage)
+    
+  });
+ 
   // remove item from app
 
   // listen for click event (del)
@@ -25,6 +55,20 @@ $(document).ready(function(){
     // clear local storage
     localStorage.clear();
     $(".show-text").empty();
+
   });
 
+
+
+  $("ul").on("dblclick", function() {
+    console.log(event)
+    var itemName = event.srcElement.textContent;
+    for(var key in localStorage){
+      if(localStorage[key] === itemName) {
+        localStorage.removeItem(key);
+      }
+    }
+    iterator(localStorage)
+  })
+  iterator(localStorage)
 });
